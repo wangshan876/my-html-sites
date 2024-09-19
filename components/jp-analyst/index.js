785ecc33a32d0d4aff8708d2fdeacbf5d8011d7e
wangshan876 <wudangxiaodao@gmail.com>
2024-09-19 13:07:33 +0800

    jp

--- components/jp-analyst/index.js
+++ components/jp-analyst/index.js
@@ -205,7 +205,7 @@ export default class JPAnalyzer extends HTMLElement {
       kuroshiro_analyzer_script.onload = async (e) => {
         this.loadDict();
       };
-      this.shadowRoot.append( kuroshiro_analyzer_script);
+      this.shadowRoot.append(kuroshiro_analyzer_script);
     }
   }
   // 指定要观察的属性
@@ -222,7 +222,7 @@ export default class JPAnalyzer extends HTMLElement {
         console.warn("字典还未加载完成");
         return;
       }
-      const result = await this.analyzer.?parse(newValue);
+      const result = await this.analyzer?.parse(newValue);
       // const result2 = await this.analyzer.convert(newValue);
       const _format = this.format(result);
       this.shadowRoot.querySelector("#sentence").innerHTML = _format.data;
