af5b512e88c3ab604b949c3efad3650bd3e66757
wangshan876 <wudangxiaodao@gmail.com>
2024-09-25 15:56:01 +0800

    type auto focus

--- components/WritingPractice.js
+++ components/WritingPractice.js
@@ -228,10 +228,23 @@ class DictationComponent extends HTMLElement {
             });
         }
 
+        
         this.shadowRoot.querySelector('.skip').addEventListener('click', e => {
             textareaElement.value = original
             this.compare(original, original)
         })
+
+        // z自动聚焦
+        this.shadowRoot.addEventListener('keydown', (event) => {
+            if (!document.activeElement.isEqualNode(textareaElement)) {
+                const keysToHandle = ['Enter', ' ', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ...Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i))];
+                if (keysToHandle.includes(event.key)) {
+                    textareaElement.focus();
+
+                }
+            }
+        });
+        
     }
 }
 
